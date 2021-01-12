32fae7373c60a9e8e7f15677f3fe3c78d6623e27
Brandon Ochia <BOchia@cybertrol.com>
2021-01-12 03:20:01 -0600

    Add project files.

---
+++ PdfToDocxConverter/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace PdfToDocxConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectInputFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "PDF files |*.pdf"};
+            openFileDialog.ShowDialog();
+
+            var directoryPath = System.IO.Path.GetDirectoryName(openFileDialog.FileName);
+            var fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(openFileDialog.SafeFileName);
+            var filePathWithoutExtension = directoryPath + "\\" + fileNameWithoutExtension;
+
+            SautinSoft.PdfFocus f = new SautinSoft.PdfFocus();
+            f.OpenPdf(openFileDialog.FileName);
+
+            if (f.PageCount > 0)
+            {
+                f.WordOptions.Format = SautinSoft.PdfFocus.CWordOptions.eWordDocument.Docx;
+                f.ToWord($"{filePathWithoutExtension}.docx");
+            }
+        }
+
+    }
+}
